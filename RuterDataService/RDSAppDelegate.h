cb1490bef342ec00796ef28a45b723a339494268
Cameron Lowell Palmer <cameron.palmer@shortcut.no>
2014-08-17 17:36:37 +0200

    Initial commit

---
+++ RuterDataService/RDSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RDSAppDelegate.h
+//  RuterDataService
+//
+//  Created by Cameron Palmer on 17.08.14.
+//  Copyright (c) 2014 Cameron Lowell Palmer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RDSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RuterDataService/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RuterDataService
+//
+//  Created by Cameron Palmer on 17.08.14.
+//  Copyright (c) 2014 Cameron Lowell Palmer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RDSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RDSAppDelegate class]));
+    }
+}
